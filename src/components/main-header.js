6c173d8be27b12341397ce6e24e45d083805d0ac
PEDRAM <pedrawm.partow1377@gmail.com>
2021-11-29 13:33:06 +0330

    pedi

---
+++ src/components/main-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainHeader = () => {
+
+    return (
+        <>
+        <div className="headerContainer">
+
+        </div>
+        </>
+    );
+}
+export default MainHeader;
