9d306d1b8ddf9a63193d3f82fbe25a412e53c5bf
PEDRAM <pedrawm.partow1377@gmail.com>
2021-11-30 13:54:41 +0330

    147

--- src/components/main-header.js
+++ src/components/main-header.js
@@ -3,12 +3,8 @@ import React from 'react';
 const MainHeader = () => {
 
     return (
-        <>
         <div className="headerContainer">
-            <>
-            </>
         </div>
-        </>
     );
 }
 export default MainHeader;
